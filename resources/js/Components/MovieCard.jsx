501a1d5a8712a5cf6126035865667415c692b4b0
diomahardika <diomahardikaaaa@gmail.com>
2024-10-01 17:15:11 +0800

    feat: slicing detail movie

--- resources/js/Components/MovieCard.jsx
+++ resources/js/Components/MovieCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "@inertiajs/react";
 import PropType from "prop-types";
 
 MovieCard.propType = {
@@ -16,9 +17,7 @@ export default function MovieCard({ slug, name, category, thumbnail }) {
             />
             <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px] rounded-br-[28px]">
                 <div className="px-7 pb-7">
-                    <div className="font-medium text-xl text-white">
-                        {name}
-                    </div>
+                    <div className="font-medium text-xl text-white">{name}</div>
                     <p className="mb-0 text-gray-300 text-base mt-[10px]">
                         {category}
                     </p>
@@ -30,7 +29,10 @@ export default function MovieCard({ slug, name, category, thumbnail }) {
             >
                 <img src="/icons/ic_play.svg" className="" width="50" alt="" />
             </div>
-            <a href={slug} className="inset-0 absolute z-50"></a>
+            <Link
+                href={route("prototype.movie.show", slug)}
+                className="inset-0 absolute z-50"
+            ></Link>
         </div>
     );
 }
